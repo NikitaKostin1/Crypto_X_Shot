b4834375e25bc1f58350066b537ac53eba9fa51c
NikitaKostin1 <nikitos9278@gmail.com>
2023-11-23 21:19:30 +0200

    improve asyncio handling in signals thread startup

--- signals/main.py
+++ signals/main.py
@@ -11,14 +11,17 @@ def start_server():
 	"""
 	Starts the signals server in a separate thread
 	"""
-	def asynchronous_start(wait_for, loop):
-		asyncio.set_event_loop(loop)
-		loop.create_task(thread.server(wait_for))
+	def asynchronous_start(wait_for, bot_loop):
+		loop = asyncio.new_event_loop()
 
-	loop = asyncio.get_event_loop()
+		asyncio.set_event_loop(loop)
+		asyncio.run_coroutine_threadsafe(
+			thread.server(wait_for), bot_loop
+		)
+
+	bot_loop = asyncio.get_event_loop()
 	server_thread = threading.Thread(
-		target=asynchronous_start, args=(90, loop)
+		target=asynchronous_start, args=(90, bot_loop)
 	)
 	server_thread.daemon = True
 	server_thread.start()
-
